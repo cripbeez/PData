96afde0f1d6501b1af686a49f5a62957cccf8622
Johannes Backman <johannesbackman98@gmail.com>
2019-02-03 02:47:51 +0100

    sort and search

--- PData/GUI/Overview.Designer.cs
+++ PData/GUI/Overview.Designer.cs
@@ -37,10 +37,15 @@
             this.leftMenuTopPanel = new System.Windows.Forms.Panel();
             this.leftMenuTopLabel = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.menuPanel = new System.Windows.Forms.Panel();
+            this.containerPanel = new System.Windows.Forms.Panel();
+            this.searchBoxPanel = new System.Windows.Forms.Panel();
+            this.searchBoxTxt = new System.Windows.Forms.TextBox();
+            this.bottomMenuPanel = new System.Windows.Forms.Panel();
             this.leftMenu.SuspendLayout();
             this.leftMenuTopPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.containerPanel.SuspendLayout();
+            this.searchBoxPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // leftMenu
@@ -51,7 +56,7 @@
             this.leftMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.leftMenu.Location = new System.Drawing.Point(0, 0);
             this.leftMenu.Name = "leftMenu";
-            this.leftMenu.Size = new System.Drawing.Size(240, 490);
+            this.leftMenu.Size = new System.Drawing.Size(0, 490);
             this.leftMenu.TabIndex = 0;
             // 
             // leftMenuBottomPanel
@@ -60,7 +65,7 @@
             this.leftMenuBottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.leftMenuBottomPanel.Location = new System.Drawing.Point(0, 433);
             this.leftMenuBottomPanel.Name = "leftMenuBottomPanel";
-            this.leftMenuBottomPanel.Size = new System.Drawing.Size(240, 57);
+            this.leftMenuBottomPanel.Size = new System.Drawing.Size(0, 57);
             this.leftMenuBottomPanel.TabIndex = 0;
             // 
             // leftMenuTopPanel
@@ -70,18 +75,19 @@
             this.leftMenuTopPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.leftMenuTopPanel.Location = new System.Drawing.Point(0, 0);
             this.leftMenuTopPanel.Name = "leftMenuTopPanel";
-            this.leftMenuTopPanel.Size = new System.Drawing.Size(240, 60);
+            this.leftMenuTopPanel.Size = new System.Drawing.Size(0, 60);
             this.leftMenuTopPanel.TabIndex = 1;
             // 
             // leftMenuTopLabel
             // 
+            this.leftMenuTopLabel.AutoEllipsis = true;
             this.leftMenuTopLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
             this.leftMenuTopLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.leftMenuTopLabel.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.leftMenuTopLabel.ForeColor = System.Drawing.Color.White;
             this.leftMenuTopLabel.Location = new System.Drawing.Point(0, 0);
             this.leftMenuTopLabel.Name = "leftMenuTopLabel";
-            this.leftMenuTopLabel.Size = new System.Drawing.Size(240, 60);
+            this.leftMenuTopLabel.Size = new System.Drawing.Size(0, 60);
             this.leftMenuTopLabel.TabIndex = 0;
             this.leftMenuTopLabel.Text = "Overview";
             this.leftMenuTopLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -113,10 +119,10 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
             this.dataGridView1.EnableHeadersVisualStyles = false;
             this.dataGridView1.GridColor = System.Drawing.Color.Silver;
-            this.dataGridView1.Location = new System.Drawing.Point(240, 0);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
@@ -144,18 +150,58 @@
             this.dataGridView1.ShowCellToolTips = false;
             this.dataGridView1.ShowEditingIcon = false;
             this.dataGridView1.ShowRowErrors = false;
-            this.dataGridView1.Size = new System.Drawing.Size(635, 490);
+            this.dataGridView1.Size = new System.Drawing.Size(875, 490);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.dataGridView1_DataBindingComplete);
+            this.dataGridView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyDown);
             // 
-            // menuPanel
+            // containerPanel
             // 
-            this.menuPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.menuPanel.Location = new System.Drawing.Point(240, 161);
-            this.menuPanel.Name = "menuPanel";
-            this.menuPanel.Size = new System.Drawing.Size(635, 329);
-            this.menuPanel.TabIndex = 1;
-            this.menuPanel.Visible = false;
+            this.containerPanel.Controls.Add(this.searchBoxPanel);
+            this.containerPanel.Controls.Add(this.bottomMenuPanel);
+            this.containerPanel.Controls.Add(this.leftMenu);
+            this.containerPanel.Controls.Add(this.dataGridView1);
+            this.containerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.containerPanel.Location = new System.Drawing.Point(0, 0);
+            this.containerPanel.Name = "containerPanel";
+            this.containerPanel.Size = new System.Drawing.Size(875, 490);
+            this.containerPanel.TabIndex = 1;
+            // 
+            // searchBoxPanel
+            // 
+            this.searchBoxPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.searchBoxPanel.Controls.Add(this.searchBoxTxt);
+            this.searchBoxPanel.Location = new System.Drawing.Point(12, 12);
+            this.searchBoxPanel.Name = "searchBoxPanel";
+            this.searchBoxPanel.Padding = new System.Windows.Forms.Padding(0, 20, 20, 20);
+            this.searchBoxPanel.Size = new System.Drawing.Size(325, 63);
+            this.searchBoxPanel.TabIndex = 2;
+            this.searchBoxPanel.Visible = false;
+            // 
+            // searchBoxTxt
+            // 
+            this.searchBoxTxt.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchBoxTxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.searchBoxTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.searchBoxTxt.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.searchBoxTxt.ForeColor = System.Drawing.Color.White;
+            this.searchBoxTxt.Location = new System.Drawing.Point(21, 23);
+            this.searchBoxTxt.Name = "searchBoxTxt";
+            this.searchBoxTxt.Size = new System.Drawing.Size(281, 18);
+            this.searchBoxTxt.TabIndex = 0;
+            this.searchBoxTxt.TextChanged += new System.EventHandler(this.searchBoxTxt_TextChanged);
+            this.searchBoxTxt.KeyDown += new System.Windows.Forms.KeyEventHandler(this.searchBoxTxt_KeyDown);
+            // 
+            // bottomMenuPanel
+            // 
+            this.bottomMenuPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomMenuPanel.Location = new System.Drawing.Point(0, 227);
+            this.bottomMenuPanel.Name = "bottomMenuPanel";
+            this.bottomMenuPanel.Size = new System.Drawing.Size(875, 263);
+            this.bottomMenuPanel.TabIndex = 1;
+            this.bottomMenuPanel.Visible = false;
             // 
             // Overview
             // 
@@ -164,20 +210,25 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
             this.ClientSize = new System.Drawing.Size(875, 490);
             this.ControlBox = false;
-            this.Controls.Add(this.menuPanel);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.leftMenu);
+            this.Controls.Add(this.containerPanel);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Overview";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Overview";
+            this.TransparencyKey = System.Drawing.Color.Cyan;
             this.Load += new System.EventHandler(this.Overview_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Overview_KeyDown);
             this.leftMenu.ResumeLayout(false);
             this.leftMenuTopPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.containerPanel.ResumeLayout(false);
+            this.searchBoxPanel.ResumeLayout(false);
+            this.searchBoxPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -189,6 +240,9 @@
         private System.Windows.Forms.Panel leftMenuBottomPanel;
         private System.Windows.Forms.Label leftMenuTopLabel;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Panel menuPanel;
+        private System.Windows.Forms.Panel containerPanel;
+        private System.Windows.Forms.Panel bottomMenuPanel;
+        private System.Windows.Forms.TextBox searchBoxTxt;
+        private System.Windows.Forms.Panel searchBoxPanel;
     }
 }
--- PData/GUI/Overview.cs
+++ PData/GUI/Overview.cs
@@ -15,46 +15,6 @@ namespace PData.GUI
         public Overview()
         {
             InitializeComponent();
-            // do stuff here later
-        }
-        private void Overview_Load(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Normal;
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.WindowState = FormWindowState.Maximized;
-
-            Data.Manager.Setup();
-            this.dataGridView1.DataSource = Data.Manager.TestTable;
-
-            foreach (Control control in this.Controls)
-                control.KeyPress += (ks, ke) =>
-                {
-                    if (ke.KeyChar == (char)Keys.Escape)
-                        this.Close();
-                };
-
-            dataGridView1.KeyPress += (ks, ke) =>
-            {
-                if (ks is DataGridView dgv)
-                {
-                    bool handled = false;
-
-                    switch ((Keys)ke.KeyChar)
-                    {
-                        case Keys.Enter:
-                            menuPanel.Show();
-                            handled = true;
-                            break;
-
-                        case Keys.Back:
-                            menuPanel.Hide();
-                            handled = false;
-                            break;
-                    }
-
-                    ke.Handled = handled;
-                }
-            };
         }
 
         public new void Show()
@@ -62,6 +22,141 @@ namespace PData.GUI
             base.ShowDialog();
         }
 
+        public void ShowLeftMenu()
+        {
+            if (leftMenu.Width > 0)
+                return;
+
+            Timer showTimer = new Timer();
+            showTimer.Interval = 10;
+            showTimer.Tick += (sender, e) =>
+            {
+                if (sender is Timer timer)
+                {
+                    if (leftMenu.Width < 230)
+                    {
+                        leftMenu.Width += 10;
+                    }
+                    else
+                    {
+                        timer.Stop();
+                    }
+                }
+            };
+            showTimer.Start();
+        }
+        public void HideLeftMenu()
+        {
+            if (leftMenu.Width < 230)
+                return;
+
+            Timer showTimer = new Timer();
+            showTimer.Interval = 10;
+            showTimer.Tick += (sender, e) =>
+            {
+                if (sender is Timer timer)
+                {
+                    if (leftMenu.Width > 0)
+                    {
+                        leftMenu.Width -= 10;
+                    }
+                    else
+                    {
+                        timer.Stop();
+                    }
+                }
+            };
+            showTimer.Start();
+        }
+        public void ToggleLeftMenu()
+        {
+            if (leftMenu.Width == 230)
+                HideLeftMenu();
+
+            if (leftMenu.Width == 0)
+                ShowLeftMenu();
+
+            return;
+        }
+
+        public void ShowBottomMenu()
+        {
+            if (bottomMenuPanel.Visible)
+                return;
+
+            dataGridView1.Enabled = false;
+            bottomMenuPanel.Show();
+        }
+        public void HideBottomMenu()
+        {
+            if (!bottomMenuPanel.Visible)
+                return;
+
+            dataGridView1.Enabled = true;
+            bottomMenuPanel.Hide();
+        }
+
+        public void ShowSearchMenu()
+        {
+            Point loc = new Point();
+            loc.X = this.Width - searchBoxPanel.Width - 5;
+            loc.Y = 5;
+
+            searchBoxPanel.Location = loc;
+            dataGridView1.Enabled = false;
+            this.KeyPreview = false;
+            searchBoxPanel.Show();
+            searchBoxTxt.Focus();
+        }
+        public void HideSearchMenu()
+        {
+            dataGridView1.Enabled = true;
+            dataGridView1.Focus();
+            this.KeyPreview = true;
+            searchBoxPanel.Hide();
+        }
+
+        private int CurrentColumn = 0;
+        private void MoveColumnLeft()
+        {
+            int next = CurrentColumn - 1;
+
+            if (next >= 0)
+            {
+                CurrentColumn = next;
+                SortColumnAsc();
+            }
+        }
+        private void MoveColumnRight()
+        {
+            int next = CurrentColumn + 1;
+
+            if (next < dataGridView1.Columns.Count)
+            {
+                CurrentColumn = next;
+                SortColumnAsc();
+            }
+        }
+        private void SortColumnAsc()
+        {
+            dataGridView1.Sort(dataGridView1.Columns[CurrentColumn], ListSortDirection.Ascending);
+        }
+        private void SortColumnDesc()
+        {
+            dataGridView1.Sort(dataGridView1.Columns[CurrentColumn], ListSortDirection.Descending);
+        }
+
+        private void Overview_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            Cursor.Hide();
+
+            Data.Manager.Setup();
+            this.dataGridView1.DataSource = Data.Manager.TestTable;
+        }
         private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
         {
             if (sender is DataGridView dataGridView)
@@ -71,6 +166,106 @@ namespace PData.GUI
                 foreach (DataGridViewColumn col in dataGridView.Columns)
                     col.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             }
+        }
+        private void Overview_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool handled = false;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    ShowBottomMenu();
+                    handled = true;
+                    break;
+
+                case Keys.Back:
+                    HideBottomMenu();
+                    dataGridView1.Focus();
+                    handled = true;
+                    break;
+
+                case Keys.M:
+                    ToggleLeftMenu();
+                    handled = true;
+                    break;
+
+                case Keys.F:
+                    ShowSearchMenu();
+                    break;
+
+                case Keys.Escape:
+                    this.Close();
+                    break;
+            }
+
+            /* column sorting */
+            switch (e.KeyCode)
+            {
+                case Keys.NumPad4:
+                    MoveColumnLeft();
+                    handled = true;
+                    break;
+
+                case Keys.NumPad6:
+                    MoveColumnRight();
+                    handled = true;
+                    break;
+
+                case Keys.NumPad8:
+                    SortColumnAsc();
+                    handled = true;
+                    break;
+
+                case Keys.NumPad2:
+                    SortColumnDesc();
+                    handled = true;
+                    break;
+
+            }
+
+            if (handled)
+                e.SuppressKeyPress = true;
+
+            e.Handled = handled;
+        }
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender is DataGridView view)
+                if (e.KeyCode == Keys.Enter)
+                    e.SuppressKeyPress = true;
+        }
+        private void searchBoxTxt_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = searchBoxTxt.Text;
+
+            if(searchText == "")
+            {
+                (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = "";
+                return;
+            }
+
+            StringBuilder filter = new StringBuilder();
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                if (filter.ToString() == "")
+                {
+                    filter.Append("[" + column.Name + "] like '%" + searchText + "%'");
+                }
+                else
+                {
+                    filter.Append(" OR ");
+                    filter.Append("[" + column.Name + "] like '%" + searchText + "%'");
+                }
+            }
+            
+            (dataGridView1.DataSource as DataTable).DefaultView.RowFilter = filter.ToString();
+        }
+        private void searchBoxTxt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (sender is TextBox box)
+                if (e.KeyCode == Keys.Enter)
+                    HideSearchMenu();
         }
     }
 }
